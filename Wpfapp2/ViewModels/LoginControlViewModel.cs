3dbb5309576a9e5dd60e52597d750a17bb60bde8
GoSeongCheol <tjdcjf0781@gmail.com>
2025-04-12 20:30:27 +0900

    control

--- Wpfapp2/ViewModels/LoginControlViewModel.cs
+++ Wpfapp2/ViewModels/LoginControlViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Wpfapp2.ViewModels
 {
-    class LoginControlViewModel
+    [ObservableObject]
+    public partial class LoginControlViewModel
     {
+
     }
 }
--- Wpfapp2/ViewModels/MainViewModel.cs
+++ Wpfapp2/ViewModels/MainViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Wpfapp2.ViewModels
 {
-    class MainViewModel
+    [ObservableObject]
+    public partial class MainViewModel
     {
+        [ObservableProperty]
+        private INotifyPropertyChanged currentViewModel; 
+
+        [RelayCommand]
+        public void ToLogin()
+        {
+            CurrentViewModel = (INotifyPropertyChanged)App.Current.Services.GetService(typeof(LoginControlViewModel))!;
+        }
+
+        [RelayCommand]
+        public void ToChangePwd()
+        {
+            CurrentViewModel = (INotifyPropertyChanged)App.Current.Services.GetService(typeof(ChangePwdControlViewModel))!;
+        }
+
+        [RelayCommand]
+        public void ToSignup()
+        {
+            CurrentViewModel = (INotifyPropertyChanged)App.Current.Services.GetService(typeof(SignupControlViewModel))!;
+        }
+
+        public MainViewModel() // 생성자
+        {
+            currentViewModel = (INotifyPropertyChanged)App.Current.Services.GetService(typeof(LoginControlViewModel))!;
+        }
+
+
+
     }
 }
