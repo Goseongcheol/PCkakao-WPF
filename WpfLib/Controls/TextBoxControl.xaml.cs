ed35da695104378f80271d53d362629961a25a0b
GoSeongCheol <tjdcjf0781@gmail.com>
2025-04-18 00:40:37 +0900

    TextBox Control

--- WpfLib/Controls/TextBoxControl.xaml.cs
+++ WpfLib/Controls/TextBoxControl.xaml.cs
@@ -24,5 +24,72 @@ namespace WpfLib.Controls
         {
             InitializeComponent();
         }
+
+
+
+        public bool Validating
+        {
+            get { return (bool)GetValue(ValidatingProperty); }
+            set { SetValue(ValidatingProperty, value); }
+        }     
+
+        public new Brush BorderBrush
+        {
+            get { return (Brush)GetValue(BorderBrushProperty); }
+            set { SetValue(BorderBrushProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public Brush WaterMarkTextColor
+        {
+            get { return (Brush)GetValue(WaterMarkTextColorProperty); }
+            set { SetValue(WaterMarkTextColorProperty, value); }
+        }
+
+        public string WaterMarkText
+        {
+            get { return (string)GetValue(WaterMarkTextProperty); }
+            set { SetValue(WaterMarkTextProperty, value); }
+        }
+
+        public new Thickness BorderThickness
+        {
+            get { return (Thickness)GetValue(BorderThicknessProperty); }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static new readonly DependencyProperty BorderBrushProperty =
+            DependencyProperty.Register("BorderBrush", typeof(Brush), typeof(TextBoxControl), new UIPropertyMetadata(Brushes.SkyBlue));
+
+        // Using a DependencyProperty as the backing store for BorderThickness.  This enables animation, styling, binding, etc...
+        public static new readonly DependencyProperty BorderThicknessProperty =
+            DependencyProperty.Register("BorderThickness", typeof(Thickness), typeof(TextBoxControl), new UIPropertyMetadata(new Thickness(1)));
+
+        // Using a DependencyProperty as the backing store for WaterMarkText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WaterMarkTextProperty =
+            DependencyProperty.Register("WaterMarkText", typeof(string), typeof(TextBoxControl), new PropertyMetadata(null));
+
+        // Using a DependencyProperty as the backing store for WaterMarkTextColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WaterMarkTextColorProperty =
+            DependencyProperty.Register("WaterMarkTextColor", typeof(Brush), typeof(TextBoxControl), new UIPropertyMetadata(Brushes.Gray));
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextBoxControl), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        // Using a DependencyProperty as the backing store for Validating.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValidatingProperty =
+            DependencyProperty.Register("Validating", typeof(bool), typeof(TextBoxControl), new UIPropertyMetadata(false));
+
+
+
+
     }
 }
